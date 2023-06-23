2193afcd6b0829e498f67ce92315ee5a56617ab6
romankoch35 <Kochetkov81@yandex.ru>
2023-06-24 02:36:50 +0300

    deleted

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int balance = 13676;
+        int miles = balance / 20;
+
+        System.out.println("Ваш баланс: " + miles + " миль!");
+
     }
 }
